935f96071abc9f48876e63c979f61331d510b1a4
yariks5s <bryuh_yaroslaw@knu.ua>
2022-11-20 00:33:48 +0100

    v.1

---
+++ Homework8/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+class Rectangle : Square
+{
+    public int Width { get; set; }
+    public int Height { get; set; }
+    public int GetRectangleArea()
+    {
+        return Width * Height;
+    }
+}
+
+//квадрат наслідується від прямокутника!!!
+class Square
+{
+    public virtual int Width
+    {
+        get { return base.Width; }
+        set
+        {
+            base.Height = value;
+            base.Width = value;
+        }
+    }
+    public virtual int Height
+    {
+        get { return base.Height; }
+        set
+        {
+            base.Height = value;
+            base.Width = value;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rect = new Rectangle();
+            rect.Width = 5;
+            rect.Height = 10;
+
+            Console.WriteLine(rect.GetRectangleArea());
+            //Відповідь 100? Що не так???
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task3/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+class Rectangle : Square
+{
+    public int Width { get; set; }
+    public int Height { get; set; }
+    public int GetRectangleArea()
+    {
+        return Width * Height;
+    }
+}
+
+//квадрат наслідується від прямокутника!!!
+class Square
+{
+    public int Width
+    {
+        get{return Width;}
+        set
+        {
+            Height = value;
+            Width = value;
+            
+        }
+
+    }
+    public int Height
+    {
+        get { return Height; }
+        set
+        {
+            Height = value;
+            Width = value;
+        }
+       
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rect = new Rectangle();
+            rect.Width = 5;
+            rect.Height = 10;
+
+            Console.WriteLine(rect.GetRectangleArea());
+            //Відповідь 100? Що не так???
+            Console.ReadKey();
+        }
+    }
+}
